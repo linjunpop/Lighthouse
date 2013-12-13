9585b7ba64bc66ed3abf35b18b39a43caf8b168d
Jun Lin <linjunpop@gmail.com>
2013-12-13 16:10:58 +0800

    Init.

--- Lighthouse/ViewController.m
+++ Lighthouse/ViewController.m
@@ -6,9 +6,17 @@
 //  Copyright (c) 2013 Sumi Interactive. All rights reserved.
 //
 
+#import <CoreLocation/CoreLocation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (strong, nonatomic) CLBeaconRegion *beaconRegion;
+@property (strong, nonatomic) NSDictionary *beaconPeripheralData;
+@property (strong, nonatomic) CBPeripheralManager *peripheralManager;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -17,6 +25,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self initRegion];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +36,75 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initRegion {
+    NSUUID *proximityUUID = [[NSUUID alloc] initWithUUIDString:@"D37112AC-DFAA-546C-8EF3-C94AA33845B5"];
+    self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:proximityUUID identifier:@"com.devfright.myRegion"];
+    [self.locationManager startMonitoringForRegion:self.beaconRegion];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    [self.locationManager startRangingBeaconsInRegion:self.beaconRegion];
+    [self notify];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+    [self.locationManager stopRangingBeaconsInRegion:self.beaconRegion];
+}
+
+- (void) locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
+{
+    [self.locationManager requestStateForRegion:self.beaconRegion];
+}
+
+- (void) locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
+{
+    switch (state) {
+        case CLRegionStateInside:
+            [self.locationManager startRangingBeaconsInRegion:self.beaconRegion];
+            [self.label setText:@"Inside"];
+            break;
+        case CLRegionStateOutside:
+            [self.label setText:@"Outside"];
+        case CLRegionStateUnknown:
+            [self.label setText:@"Unknown"];
+        default:
+            // stop ranging beacons, etc
+            NSLog(@"Region unknown");
+    }
+}
+
+- (void) locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
+{
+    if ([beacons count] > 0) {
+        // Handle your found beacons here
+    }
+}
+
+- (void) notify
+{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+
+    // 1 sec later
+    NSDate *pushDate = [NSDate dateWithTimeIntervalSinceNow:1];
+    if (notification != nil) {
+        notification.fireDate = pushDate;
+        notification.timeZone = [NSTimeZone defaultTimeZone];
+        notification.repeatInterval = kCFCalendarUnitDay;
+        notification.soundName = UILocalNotificationDefaultSoundName;
+        notification.alertBody = @"Enter Region, Welcome";
+        notification.applicationIconBadgeNumber += 1;
+        NSDictionary *info = [NSDictionary dictionaryWithObject:@"name" forKey:@"key"];
+        notification.userInfo = info;
+        UIApplication *app = [UIApplication sharedApplication];
+        [app scheduleLocalNotification:notification];
+    }
+}
+
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification*)notification
+{
+    application.applicationIconBadgeNumber -= 1;
 }
 
 @end
