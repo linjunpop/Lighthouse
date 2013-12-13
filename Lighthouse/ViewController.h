48fd028f40ba040b69d587732f69bfbd387c9567
Jun Lin <linjunpop@gmail.com>
2013-12-13 16:02:36 +0800

    Initial Commit

---
+++ Lighthouse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lighthouse
+//
+//  Created by Jun Lin on 12/13/13.
+//  Copyright (c) 2013 Sumi Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
